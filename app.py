90598cf023886167c20c1dbe6e44445fb6418844
Somasree Majumder <bishnupadamajumder32@gmail.com>
2023-04-30 17:23:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
 
+import gradio as gr
+
+def greet(name):
+    return "Hello " + name + "!"
+
+demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+    
+if __name__ == "__main__":
+    demo.launch()   
